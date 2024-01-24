f4cb3318b60da4306f6512653b1af8315fc525b9
Thomas Wang <wang.thomasdavid@gmail.com>
2024-01-24 16:15:26 -0500

    Initial Commit

---
+++ VacScan/VacScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacScanApp.swift
+//  VacScan
+//
+//  Created by Thomas Wang on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct VacScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
